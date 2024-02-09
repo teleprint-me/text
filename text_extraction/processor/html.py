5a82a04cddd242e89a1b0382a9bdb340d2fed2ac
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-02-09 16:42:20 -0500

    refactor: Clean up process_entry for readability and maintainability

--- text_extraction/processor/html.py
+++ text_extraction/processor/html.py
@@ -58,12 +58,10 @@ def replace_code_tags_with_backticks(markdown_text: str) -> str:
 
 def process_entry(file_entry: os.DirEntry, pbar: tqdm.tqdm, dry_run: bool) -> None:
     """Process a single directory entry (file) and convert it to Markdown if needed."""
-    md = MarkdownIt()
-    output_path = os.path.join("markdown_dataset", file_entry.path)
-
+    logger = get_default_logger(__name__, logging.INFO)
     html_content = FileManager.read(file_entry.path)
     if html_content is None:
-        get_default_logger(__name__, logging.ERROR).error(
+        logger.error(
             f"An error occurred while reading the file {file_entry.name}. Skipping."
         )
         return
@@ -71,12 +69,13 @@ def process_entry(file_entry: os.DirEntry, pbar: tqdm.tqdm, dry_run: bool) -> No
     html_content = clean_code_blocks(html_content)
     markdown_content = convert_html_to_markdown(html_content)
     markdown_content = markdown_content.replace(".html", ".md")
-    output_path = output_path.replace(".html", ".md")
+    output_path = os.path.join("markdown_dataset", file_entry.path).replace(
+        ".html", ".md"
+    )
 
-    if not dry_run:
-        FileManager.write(output_path, markdown_content)
-        pbar.update(1)
-    else:
-        get_default_logger(__name__, logging.INFO).info(
-            f"Would write {len(markdown_content)} bytes to {output_path}"
-        )
+    if dry_run:
+        logger.info(f"Would write {len(markdown_content)} bytes to {output_path}")
+        return
+
+    FileManager.write(output_path, markdown_content)
+    pbar.update(1)
