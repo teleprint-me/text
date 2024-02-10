fea96c78e186947db6c2b20edb6cb95912e4cecd
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-02-10 17:16:53 -0500

    feat: Add support for handling files or directories along with a optional output dir path

--- text_extraction/cli/html.py
+++ text_extraction/cli/html.py
@@ -11,12 +11,13 @@ Copyright (C) 2024 Austin Berrio
 import argparse
 import logging
 import os
-import time
+from pathlib import Path
 
-from text_extraction.processor.html import collect_files, traverse_directory
-
-# Initialize logging
-logging.basicConfig(level=logging.INFO)
+from text_extraction.logger import get_default_logger
+from text_extraction.processor.html import (
+    process_html_directory,
+    process_html_file_entry,
+)
 
 
 def get_arguments() -> argparse.Namespace:
@@ -24,36 +25,59 @@ def get_arguments() -> argparse.Namespace:
         description="Convert HTML files to Markdown format."
     )
     parser.add_argument(
-        "-p",
-        "--dir-path",
+        "-i",
+        "--input-path",
         required=True,
         default="",
-        help="Path to the directory to process.",
+        help="Path to the file or directory to process.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-dir",
+        default="converted_md",
+        help="Directory to the processed output.",
     )
     parser.add_argument(
         "-t",
         "--n-threads",
+        type=int,
         default=os.cpu_count() or 4,
         help="Number of threads to use for processing.",
     )
     parser.add_argument(
-        "-r",
+        "-d",
         "--dry-run",
-        action="store_true",
         default=False,
+        action="store_true",
         help="Perform a dry run and fake generating the C and C++ raw dataset.",
     )
     return parser.parse_args()
 
 
 def main(args: argparse.Namespace):
-    logging.info("Starting main function.")
-    start_time = time.time()
-    file_entry_list = collect_files(args.dir_path)
-    traverse_directory(file_entry_list, args.n_threads, args.dry_run)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    logging.info(f"Elapsed {elapsed_time:.2f} seconds using {args.n_threads} threads.")
+    input_path = Path(args.input_path)
+    output_path = Path(args.output_dir)
+    n_threads = args.n_threads
+    dry_run = args.dry_run
+    logger = get_default_logger(__name__, logging.INFO)
+
+    if input_path.is_file():
+        process_html_file_entry(
+            file_entry=input_path,
+            output_dir=output_path,
+            dry_run=dry_run,
+            logger=logger,
+        )
+    elif input_path.is_dir():
+        process_html_directory(
+            input_dir=input_path,
+            output_dir=output_path,
+            n_threads=n_threads,
+            dry_run=dry_run,
+            logger=logger,
+        )
+    else:
+        logger.error(f"Invalid input path: {input_path}. Exiting.")
 
 
 if __name__ == "__main__":
