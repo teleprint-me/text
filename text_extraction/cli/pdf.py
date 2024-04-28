131fe35110df8351f76f09f5e452a23afe72de9d
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2024-04-27 23:06:28 -0400

    refactor: Merge pdf module into pdf script

--- text_extraction/cli/pdf.py
+++ text_extraction/cli/pdf.py
@@ -1,5 +1,5 @@
 """
-text_extraction/cli/pdf.py
+text_extraction/pdf.py
 
 A Python script for converting PDF files to plaintext.
 
@@ -8,8 +8,28 @@ Copyright (C) 2024 Austin Berrio
 
 import argparse
 import os
+from pathlib import Path
+from typing import List, Union
 
-from text_extraction.processor.pdf import convert_pdf_to_text
+from poppler import load_from_file
+
+
+def convert_pdf_to_text(file_name: Union[str, Path]) -> List[str]:
+    """
+    Convert a PDF document into a list of strings, where each string represents the text of a page.
+
+    Args:
+        file_name (Union[str, Path]): The path to the PDF file to be converted.
+
+    Returns:
+        List[str]: A list of strings representing the text of each page in the PDF.
+    """
+    pages: List[str] = []
+    pdf_document = load_from_file(file_name=file_name)
+    for index in range(pdf_document.pages):
+        page = pdf_document.create_page(index)
+        pages.append(page.text())
+    return pages
 
 
 def get_arguments() -> argparse.Namespace:
