81da38552f68e3cf0695442730d4ea75288e5098
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2025-01-05 01:28:44 -0500

    web(feat): Add common options for flexible html2text parsing

--- text_extraction/cli/web.py
+++ text_extraction/cli/web.py
@@ -77,11 +77,19 @@ class WebsiteFetcher:
         if settings is None:
             settings = {}
         h = html2text.HTML2Text()
-        h.wrap_links = settings.get("wrap_links", True)
-        h.single_line_break = settings.get("single_line_break", True)
-        h.mark_code = settings.get("mark_code", True)
-        h.wrap_list_items = settings.get("wrap_list_items", True)
-        h.wrap_tables = settings.get("wrap_tables", True)
+        h.unicode_snob = settings.get("unicode_snob", False)
+        h.escape_snob = settings.get("escape_snob", False)
+        h.body_width = settings.get("body_width", 79)
+        h.inline_links = settings.get("inline_links", False)
+        h.protect_links = settings.get("protect_links", False)
+        h.wrap_links = settings.get("wrap_links", False)
+        h.bypass_tables = settings.get("bypass_tables", False)
+        h.single_line_break = settings.get("single_line_break", False)
+        h.mark_code = settings.get("mark_code", False)
+        h.wrap_list_items = settings.get("wrap_list_items", False)
+        h.wrap_tables = settings.get("wrap_tables", False)
+        h.open_quote = settings.get("open_quote", '"')
+        h.close_quote = settings.get("close_quote", '"')
         return h.handle(html).strip()
 
 
@@ -179,23 +187,68 @@ def get_arguments() -> argparse.Namespace:
         help="Output the resulting markdown content to standard output",
     )
     parser.add_argument(
-        "--wrap-links", action="store_false", help="Wrap links. Default is True."
+        "--unicode-snob",
+        action="store_true",
+        help="Use Unicode instead of ASCII. Default is False.",
+    )
+    parser.add_argument(
+        "--escape-snob",
+        action="store_true",
+        help="Escape all special characters. Default is False.",
+    )
+    parser.add_argument(
+        "--body-width",
+        type=int,
+        default=79,
+        help="Wrap long lines to position. Default is 79.",
+    )
+    parser.add_argument(
+        "--inline-links",
+        action="store_true",
+        help="Use inline formatting for images and links. Default is False.",
+    )
+    parser.add_argument(
+        "--protect-links",
+        action="store_true",
+        help="Protect links from line breaks. Default is False.",
+    )
+    parser.add_argument(
+        "--wrap-links", action="store_true", help="Wrap links. Default is False."
+    )
+    parser.add_argument(
+        "--bypass-tables",
+        action="store_true",
+        help="Use HTML instead of Markdown. Default is False.",
     )
     parser.add_argument(
         "--single-line-break",
-        action="store_false",
-        help="Single line break. Default is True.",
+        action="store_true",
+        help="Add a single line break between blocks. Default is False.",
     )
     parser.add_argument(
-        "--mark-code", action="store_false", help="Mark code. Default is True."
+        "--mark-code",
+        action="store_true",
+        help="Mark code with code blocks. Default is False.",
     )
     parser.add_argument(
         "--wrap-list-items",
-        action="store_false",
-        help="Wrap list items. Default is True.",
+        action="store_true",
+        help="Wrap list items. Default is False.",
     )
     parser.add_argument(
-        "--wrap-tables", action="store_false", help="Wrap tables. Default is True."
+        "--wrap-tables", action="store_true", help="Wrap tables. Default is False."
+    )
+    parser.add_argument(
+        "--open-quote",
+        type=str,
+        default='"',
+        help="Substitute <q> tags with double quotation. Default is '\"'.",
+    )
+    parser.add_argument(
+        "--close-quote",
+        type=str,
+        default='"',
+        help="Substitute </q> tags with double quotation. Default is '\"'.",
     )
     return parser.parse_args()
 
@@ -207,11 +260,19 @@ def main():
     markdown_path, markdown_content = website_manager.get(
         args.url,
         {
+            "unicode_snob": args.unicode_snob,
+            "escape_snob": args.escape_snob,
+            "body_width": args.body_width,
+            "inline_links": args.inline_links,
+            "protect_links": args.protect_links,
             "wrap_links": args.wrap_links,
+            "bypass_tables": args.bypass_tables,
             "single_line_break": args.single_line_break,
             "mark_code": args.mark_code,
             "wrap_list_items": args.wrap_list_items,
             "wrap_tables": args.wrap_tables,
+            "open_quote": args.open_quote,
+            "close_quote": args.close_quote,
         },
     )
     if args.stdout:
